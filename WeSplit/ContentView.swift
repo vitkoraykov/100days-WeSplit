47ed9a6a29de2ec040228a16c48b76031df58510
Vito Raykov <vitko.raykov@flyt.io>
2020-03-10 19:02:05 +0000

    initial commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Vitko Raykov on 10/03/2020.
+//  Copyright © 2020 Vitko Raykov - Flyt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = ""
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 0
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var orderTotal: Double {
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                    }
+                
+                Section(header: Text("How much tips are you looking to leave?")) {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Order Total(inc. tips)")) {
+                    Text("$\(orderTotal, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                    .bold()
+                    .padding()
+                }
+            }
+            .navigationBarTitle("WeSplit", displayMode: .inline)
+        
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
